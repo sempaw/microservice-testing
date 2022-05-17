d1a66877e311b39698eeaf113ddef1d14f118d4c
Anton Orekhovskiy <anton.o.0619@gmail.com>
2022-05-17 04:08:36 +0300

    integrating repo in contract endpoint

--- app/api/api_v1/routers/contract.py
+++ app/api/api_v1/routers/contract.py
@@ -1,11 +1,14 @@
-from typing import List
-from fastapi import APIRouter, status, HTTPException
+from typing import List, Optional
+from fastapi import APIRouter, status, HTTPException, Depends
+from sqlalchemy.orm import Session
 
+from app import deps
 from app.core.contract_validator import ContractValidator
 from app.schemas.contract import Contract
+from app.models.contract import Contract as ContractModel
 from app.exceptions.not_found_exception import NotFoundException
 from app.exceptions.validation_exception import ValidationException
-import app.repositories.contracts_repository as contract_repository
+from app.repositories.contracts_repository import contract as repository
 
 
 router = APIRouter(
@@ -15,35 +18,35 @@ router = APIRouter(
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
-async def get_all() -> List[Contract]:
+async def get_all(
+        *,
+        skip: Optional[int] = None,
+        limit: Optional[int] = None,
+        db: Session = Depends(deps.get_db),
+) -> List[Contract]:
     """
     Fetch all contracts
     """
-
-    return contract_repository.get_all()
-
+    return repository.get_multi(skip=skip, limit=limit, db=db)
 
 
 @router.get("/{contract_id}", status_code=status.HTTP_200_OK)
-async def get_by_id(contract_id: int) -> Contract:
+async def get_by_id(*, contract_id: int, db: Session = Depends(deps.get_db)) -> Contract:
     """
     Fetch a single contract by ID
     """
-
-    return contract_repository.get_by_id(contract_id)
+    return repository.get(obj_id=contract_id, db=db)
 
 
 # TODO: does router supports routes only or auth and validation as well?
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def post(contract: Contract) -> int:
+async def post(contract: Contract, db: Session = Depends(deps.get_db)) -> ContractModel:
     """
     Post new contract
     """
-
     try:
         ContractValidator.validate_contract(contract.data)
-        contract_repository.create(contract)
-        return contract_repository.create(contract)
+        return repository.create(obj_in=contract, db=db)
     except NotFoundException as e:
         raise HTTPException(status_code=404, detail=str(e))
     except ValidationException as e:
@@ -51,14 +54,13 @@ async def post(contract: Contract) -> int:
 
 
 @router.put('/{contract_id}', status_code=status.HTTP_204_NO_CONTENT)
-async def update(contract: Contract, contract_id: int):
+async def update(contract: Contract, contract_id: int, db: Session = Depends(deps.get_db)):
     """
     Update whole contract by ID
     """
-
     try:
         ContractValidator.validate_contract(contract.data)
-        contract_repository.update(contract_id, contract)
+        repository.update(obj_id=contract_id, obj_in=contract, db=db)
     except NotFoundException as e:
         raise HTTPException(status_code=404, detail=str(e))
     except ValidationException as e:
@@ -66,9 +68,8 @@ async def update(contract: Contract, contract_id: int):
 
 
 @router.delete('/{contract_id}')
-async def delete(contract_id: int):
+async def delete(contract_id: int, db: Session = Depends(deps.get_db)):
     """
     Delete contract by ID
     """
-
-    contract_repository.delete(contract_id)
+    repository.delete(obj_id=contract_id, db=db)
